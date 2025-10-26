ac21c2a620a962f5eabacf3bfb00575c94697872
MrAfitol <136744279+Oleksichek@users.noreply.github.com>
2025-10-26 21:02:50 +0100

    Lab01

---
+++ Labs-OOP-WSIZ/Lab01/Animal.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01
+{
+    public class Animal
+    {
+        public string Name 
+        {
+            get => _name;
+            set {
+                _name = value;
+            }
+        }
+
+        private static int AnimalsCount = 0;
+
+        private string _name;
+        private string _species;
+        private int _lagsCount;
+
+        public Animal()
+        {
+            _name = "Poli";
+            _species = "Cat";
+            _lagsCount = 4;
+            AnimalsCount++;
+        }
+
+        public Animal(Animal animal)
+        {
+            _name = animal._name;
+            _species = animal._species;
+            _lagsCount = animal._lagsCount;
+            AnimalsCount++;
+        }
+
+        public Animal(string name, string species, int legsCount)
+        {
+            _name = name;
+            _species = species;
+            _lagsCount = legsCount;
+            AnimalsCount++;
+        }
+
+        public static int GetAnimalsCount() => AnimalsCount;
+
+        public string GetSpecies() => _species;
+        public int GetLagsCount() => _lagsCount;
+
+        public void Speak()
+        {
+            string sound = string.Empty;
+            switch (_species)
+            {
+                case "Dog":
+                    sound = "Woof!";
+                    break;
+                case "Cat":
+                    sound = "Meow!";
+                    break;
+                case "Cow":
+                    sound = "Moo!";
+                    break;
+                default:
+                    sound = string.Empty;
+                    break;
+            }
+            Console.WriteLine(sound);
+        }
+    }
+}
---
+++ Labs-OOP-WSIZ/Lab01/Program.cs
@@ -0,0 +1,65 @@
+﻿using Lab01;
+
+public class Program
+{
+    private static List<Animal> animals = new List<Animal>();
+
+    public static void Main()
+    {
+        Console.WriteLine("To jest cwiczenie 1");
+        Console.WriteLine("Nacisnij ENTER aby kontynuowac...");
+        Console.ReadLine();
+
+        for (int i = 1; i <= 3; i++)
+        {
+            Animal animal = AskForAnimal();
+            animals.Add(animal);
+            Console.WriteLine($"Dodano zwierzaka {animal.Name} do listy.");
+            Console.WriteLine("Nacisnij ENTER aby kontynuowac...");
+            Console.ReadLine();
+        }
+
+        Animal copyAnimal = new Animal(animals.Last());
+        copyAnimal.Name = "Hiko";
+        animals.Add(copyAnimal);
+
+        foreach (Animal animal in animals)
+        {
+            Console.Clear();
+            Console.WriteLine($"Nazwa: {animal.Name}");
+            Console.WriteLine($"Gatunek: {animal.GetSpecies()}");
+            Console.WriteLine($"Liczba nóg: {animal.GetLagsCount()}");
+            Console.Write("Odglos zwierzaka: ");
+            animal.Speak();
+            Console.WriteLine("Nacisnij ENTER aby kontynuowac...");
+            Console.ReadLine();
+        }
+
+        Console.Clear();
+        Console.WriteLine("Liczba zwierzaków: " + Animal.GetAnimalsCount());
+    }
+
+    private static Animal AskForAnimal()
+    {
+        Console.Clear();
+        Console.WriteLine("Podaj nazwe zwierzaka:");
+        string animalName = Console.ReadLine();
+
+        Console.Clear();
+        Console.WriteLine("Podaj gatunek zwierzaka:");
+        string animalSpecies = Console.ReadLine();
+
+        Console.Clear();
+        Console.WriteLine("Podaj liczbę nóg zwierzaka:");
+        string animalStringLagsCount = Console.ReadLine();
+
+        while (!Int32.TryParse(animalStringLagsCount, out int animalLagsCount))
+        {
+            Console.Clear();
+            Console.WriteLine("Podaj liczbę nóg zwierzaka:");
+            animalStringLagsCount = Console.ReadLine();
+        }
+
+        return new Animal(animalName, animalSpecies, Int32.Parse(animalStringLagsCount));
+    }
+}
