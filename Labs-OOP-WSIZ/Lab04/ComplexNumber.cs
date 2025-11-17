9d5ef9f16ec133df0165a2b15faa74dc4f9f59de
MrAfitol <136744279+Oleksichek@users.noreply.github.com>
2025-11-17 14:56:47 +0100

    Lab04 Part01

---
+++ Labs-OOP-WSIZ/Lab04/ComplexNumber.cs
@@ -0,0 +1,90 @@
+﻿using Lab03;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab04
+{
+    public class ComplexNumber : ICloneable, IEquatable<ComplexNumber>, IComparable<ComplexNumber>, IModular
+    {
+        public double Re
+        {
+            get { return re; }
+            set { re = value; }
+        }
+
+        public double Im
+        {
+            get { return im; }
+            set { im = value; }
+        }
+
+        private double re;
+        private double im;
+
+        public ComplexNumber(double re, double im)
+        {
+            this.re = re;
+            this.im = im;
+        }
+
+        public static ComplexNumber operator +(ComplexNumber c1, ComplexNumber c2)
+        {
+            return new ComplexNumber(c1.Re + c2.Re, c1.Im + c2.Im);
+        }
+
+        public static ComplexNumber operator -(ComplexNumber c1, ComplexNumber c2)
+        {
+            return new ComplexNumber(c1.Re - c2.Re, c1.Im - c2.Im);
+        }
+
+        public static ComplexNumber operator *(ComplexNumber c1, ComplexNumber c2)
+        {
+            return new ComplexNumber(
+                c1.Re * c2.Re - c1.Im * c2.Im,
+                c1.Re * c2.Im + c1.Im * c2.Re
+            );
+        }
+
+        public static bool operator ==(ComplexNumber c1, ComplexNumber c2) => c1.Equals(c2);
+        public static bool operator !=(ComplexNumber c1, ComplexNumber c2) => !c1.Equals(c2);
+
+        public static ComplexNumber operator -(ComplexNumber c1) => new ComplexNumber(-c1.re, -c1.im);
+
+        public static bool operator >(ComplexNumber c1, ComplexNumber c2) => c1.CompareTo(c2) > 0;
+        public static bool operator <(ComplexNumber c1, ComplexNumber c2) => c1.CompareTo(c2) < 0;
+        public static bool operator >=(ComplexNumber c1, ComplexNumber c2) => c1.CompareTo(c2) >= 0;
+        public static bool operator <=(ComplexNumber c1, ComplexNumber c2) => c1.CompareTo(c2) <= 0;
+
+        public double Modulus() => Math.Sqrt((re * re) + (im * im));
+
+        public bool Equals(ComplexNumber? equalNumber)
+        {
+            if (equalNumber is null) return false;
+            return this.Re == equalNumber.Re && this.Im == equalNumber.Im;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is ComplexNumber complexNumber)
+                return Equals(complexNumber);
+            return false;
+        }
+
+        public int CompareTo(ComplexNumber? compareNumber)
+        {
+            if (compareNumber is null) return 1;
+            return Modulus().CompareTo(compareNumber.Modulus());
+        }
+
+        public object Clone() => new ComplexNumber(this.re, this.im);
+        public override int GetHashCode() => HashCode.Combine(re, im);
+        public override string ToString()
+        {
+            string sign = im >= 0 ? "+" : "-";
+            return $"{re} {sign} {Math.Abs(im)}i";
+        }
+    }
+}
---
+++ Labs-OOP-WSIZ/Lab04/Program.cs
@@ -0,0 +1,78 @@
+﻿using Lab04;
+
+public class Program
+{
+    private static List<ComplexNumber> ComplexNumbers = new List<ComplexNumber>()
+    {
+        new ComplexNumber(1, 2),
+        new ComplexNumber(3, 4),
+        new ComplexNumber(5, 6),
+        new ComplexNumber(7, 8),
+        new ComplexNumber(9, 10)
+    };
+
+    private static List<ComplexNumber> ComplexNumbersTwo = new List<ComplexNumber>()
+    {
+        new ComplexNumber(2, 3),
+        new ComplexNumber(3, 5),
+        new ComplexNumber(4, 7),
+        new ComplexNumber(6, 7),
+        new ComplexNumber(7, 9),
+        new ComplexNumber(9, 10)
+    };
+
+    private static void Main(string[] args)
+    {
+        ShowPart01();
+        Console.WriteLine();
+        Console.WriteLine();
+        ShowPart02();
+    }
+
+    private static void ShowPart01()
+    {
+        Console.WriteLine("Default complex numbers:");
+        ShowComplexNumberList(ComplexNumbers);
+
+        ComplexNumbers.Sort();
+
+        Console.WriteLine("Sorted complex numbers:");
+        ShowComplexNumberList(ComplexNumbers);
+
+        Console.WriteLine("Min value: " + ComplexNumbers.Min());
+        Console.WriteLine("Max value: " + ComplexNumbers.Max());
+
+        ShowComplexNumberList(ComplexNumbersTwo.Where(x => x.Im > 0).ToList());
+    }
+
+    private static void ShowPart02()
+    {
+        Console.WriteLine("Default complex numbers:");
+        ShowComplexNumberList(ComplexNumbersTwo);
+
+        ComplexNumbersTwo.Sort();
+
+        Console.WriteLine("Sorted complex numbers:");
+        ShowComplexNumberList(ComplexNumbersTwo);
+
+        Console.WriteLine("Min value: " + ComplexNumbersTwo.Min());
+        Console.WriteLine("Max value: " + ComplexNumbersTwo.Max());
+
+        ShowComplexNumberList(ComplexNumbersTwo.Where(x => x.Im > 0).ToList());
+
+        ComplexNumbersTwo.RemoveAt(1);
+        ShowComplexNumberList(ComplexNumbersTwo);
+
+        ComplexNumbersTwo.Remove(ComplexNumbersTwo.Min());
+        ShowComplexNumberList(ComplexNumbersTwo);
+
+        ComplexNumbersTwo.Clear();
+        ShowComplexNumberList(ComplexNumbersTwo);
+    }
+
+    private static void ShowComplexNumberList(List<ComplexNumber> complexNumbers)
+    {
+        foreach (var compNum in ComplexNumbers)
+            Console.WriteLine(compNum);
+    }
+}
